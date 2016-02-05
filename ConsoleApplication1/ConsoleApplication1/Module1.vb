33fdeda1e8b6d413196e31dd06759623fd663e83
lucky-ei <eikichi.nohara@gmail.com>
2016-02-05 17:07:28 +0900

    2016.02.05 練習

---
+++ ConsoleApplication1/ConsoleApplication1/Module1.vb
@@ -0,0 +1,11 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("コンソールアプリテスト")
+        Console.WriteLine("リテラルはその記載内容によって型が判断されてしまう。")
+        Console.WriteLine("例えば「100」と直に書けばそれはInteger型であるし、「0.1」などと記入すればそれはDouble型である")
+        Console.WriteLine("そのためリテラルを最初から特定の型に設定する場合は、リテラルのサフィックスに特定の文字などをつける必要がある")
+        Console.WriteLine("もしDecimal型のリテラルであれば「123D」などと表現する。")
+    End Sub
+
+End Module
